edb62402ab254646ed11750e09107b5cdbf98523
Ma9rouna25 <aziz88303@gmail.com>
2026-03-18 19:00:33 +0100

    Update server.js

--- MaximoBlogFront/server.js
+++ MaximoBlogFront/server.js
@@ -4,15 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// Serve static directory where the angular build is generated
-const DIST_FOLDER = path.join(process.cwd(), 'dist/MaximoBlogFront/browser');
+const DIST_FOLDER = path.join(__dirname, '../MaximoBlogFront/dist/MaximoBlogFront/browser');
+
+console.log("Serving from:", DIST_FOLDER);
+
 app.use(express.static(DIST_FOLDER));
 
-// All other routes should redirect to index.html for Angular's HTML5 PathLocationStrategy
 app.get('*', (req, res) => {
   res.sendFile(path.join(DIST_FOLDER, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
